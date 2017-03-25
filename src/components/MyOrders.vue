66881f53677dcc6694e6ab869b3c758f19a27de0
Dracode <412507676@qq.com>
2017-03-25 12:50:50 +0800

    完成我的订单页面

--- src/components/MyOrders.vue
+++ src/components/MyOrders.vue
@@ -1,24 +1,48 @@
 <template>
-  <div class="main-container">
-    <div class="cell">
-      <section class="item" v-for="order in orderList">
-        <section class="left_box">
-          <img class="img" :src="order.goods.bannerUrl">
-        </section>
-        <section class="right_box">
-          <section class="top">{{order.goods.title}}</section>
-          <section class="middle">
-            <section>订单状态：</section>
-            <section class="item_price item_time_wait">{{order.statusDesc}}</section>
-          </section>
-        </section>
-      </section>
+  <div class="m-pmchannel-auction">
+    <div class="module-wrap">
+      <div class="auction-container square-col1">
+        <div class="auction-wrapper" v-for="order in orderList">
+          <a href="javascript:void(0);" class="item wait">
+            <div class="img-wrapper">
+              <img :src="order.goods.bannerUrl" style="width: 100%;">
+            </div>
+            <div class="info">
+              <div class="title">{{order.goods.title}}</div>
+              <div class="price-section">
+                <div class="unit">
+                  <div class="rmb">RMB</div>
+                  <br>
+                  <div class="desc">成交价</div>
+                </div>
+                <div class="price-number"><b>{{order.goods.currentPrice}}</b></div>
+                <div class="quantity"></div>
+              </div>
+              <div class="footer">
+                <div class="text-section">
+                  <div class="looker">
+                  </div>
+                  <div class="time-status">
+                    <span class="date-desc wait">
+                      {{order.statusDesc}}
+                    </span>
+                  </div>
+                </div>
+                <!--<div class="button-section">
+                  <b style="color:#970022;"></b>
+                  <div class="button-desc">立即拍</div>
+                </div>-->
+              </div>
+            </div>
+            <div class="line"></div>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
     export default{
         data(){
             return{
@@ -41,19 +65,27 @@
 
             this.$http.get("/orders").then(response => {
               response.body.items.forEach(order => {
-                order.statusDesc = orderStatusDescMap[order.status]
+                order.statusDesc = orderStatusDescMap[order.status];
                 this.$http.get("/goods/"+order.goodsId).then(response => {
                   order.goods = response.body;
                   orderList.push(order);
                 });
-              })
+              });
             });
           }
         }
     }
-
 </script>
 
 <style>
+  @import "../assets/css/goods_list.css";
+</style>
+<style>
+    html {
+        font-size: 37.5px !important;
+    }
 
+    body {
+        font-size: 14.0625px
+    }
 </style>
