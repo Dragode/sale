2ac90a1cbf6363196c096a0e70998c3e2711438d
Dracode <412507676@qq.com>
2017-03-28 08:42:18 +0800

    订单管理修改

--- src/components/OrderManage.vue
+++ src/components/OrderManage.vue
@@ -1,224 +1,107 @@
 <template>
-    <div>
-      <div v-for="order in orderList">
-        <form-preview :header-label="'订单金额'" :header-value="order.price" :body-items="order.fields" :footer-buttons="getButton(order)"></form-preview>
-        <br>
+  <div class="m-pmchannel-auction">
+    <div class="module-wrap">
+      <div class="auction-container square-col1">
+        <div class="auction-wrapper" v-for="order in orderList">
+          <step v-model="step" background-color='#fbf9fe' gutter="20px">
+            <step-item :title="'done'"></step-item>
+            <step-item :title="'processing'"></step-item>
+            <step-item :title="'end'"></step-item>
+          </step>
+          <a href="javascript:void(0);" class="item wait">
+            <div class="img-wrapper">
+              <img :src="order.goods.bannerUrl" style="width: 100%;">
+            </div>
+            <div class="info">
+              <div class="title">{{order.goods.title}}</div>
+              <div class="price-section">
+                <div class="unit">
+                  <div class="rmb">RMB</div>
+                  <br>
+                  <div class="desc">成交价</div>
+                </div>
+                <div class="price-number"><b>{{order.goods.currentPrice}}</b></div>
+                <div class="quantity"></div>
+              </div>
+              <div class="footer">
+                <div class="text-section">
+                  <div class="looker">
+                  </div>
+                  <div class="time-status">
+                    <span class="date-desc wait">
+                      {{order.user.phoneNumber}}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="line"></div>
+          </a>
+          <div class="btn_wrap">
+            <x-button type="primary" @click.native="nextStep">next step</x-button>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import { FormPreview , AlertPlugin } from 'vux'
-    import Vue from 'vue'
-    Vue.use(AlertPlugin)
+  import { Step, StepItem, XButton, XHr } from 'vux'
 
-    var test = "test";
-
-    export default{
-      components: {
-        FormPreview
+  export default{
+    components: {
+      Step,
+      StepItem,
+      XButton,
+      XHr
+    },
+    data(){
+      return{
+        orderList:[],
+        orderStatusDescMap:{
+          "WAIT_FOR_PAY":"等待支付",
+          "PAY_SUCCESS":"支付成功",
+          "DILIVERED":"已发货",
+          "FINISH":"已完成"
+        },
+        step:0
+      }
+    },
+    created:function(){
+      this.getOrders();
+    },
+    methods:{
+      nextStep(){
+        this.step ++;
       },
-      data(){
-        return{
-          orderList:[
-          ]
-        }
-      },
-      mounted:function(){
+      getOrders(){
         var orderList = this.orderList;
-        var vux = this.$vux;
-        var httpClient = this.$http;
-        this.$http.get('/orders/all').then(
-          function(response){
-            var response = response.body;
-            if("0" == response.resultCode){
-              response.items.forEach(function(order){
-                var goodsTitle;
-                var userPhoneNumber;
-                //TODO 改成Promise.all
-                httpClient.get("/goods/"+order.goodsId).then(
-                  function(response){
-                    goodsTitle = response.body.title;
-                  },
-                  function(){
-                    vux.alert.show({
-                      title: '提示',
-                      content: '获取订单失败！',
-                      onHide () {
-                          window.location.reload();
-                      }
-                    });
-                  }
-                ).then(()=>{
-                  httpClient.get("/users/"+order.userId).then(
-                    function(response){
-                      userPhoneNumber = response.body.phoneNumber;
+        var orderStatusDescMap = this.orderStatusDescMap;
 
-                      order.fields = [
-                        {
-                          label: '订单编号',
-                          value: order.displayId
-                        },
-                        {
-                          label: '用户手机号码',
-                          value: userPhoneNumber
-                        }, {
-                          label: '商品名称',
-                          value: goodsTitle
-                        }
-                      ];
-
-                      order.price = "¥" + order.price + ".00";
-
-                      orderList.push(order);
-                    },
-                    function(){
-                      vux.alert.show({
-                        title: '提示',
-                        content: '获取订单失败！',
-                        onHide () {
-                            window.location.reload();
-                        }
-                      });
-                    }
-                  );
-                  }
-                )
-              })
-            }else{
-              vux.alert.show({
-                title: '提示',
-                content: '获取订单失败！',
-                onHide () {
-                    window.history.back();
-                }
+        this.$http.get("/orders/all").then(response => {
+          response.body.items.forEach(order => {
+            order.statusDesc = orderStatusDescMap[order.status];
+            this.$http.get("/goods/"+order.goodsId).then(response => {
+              order.goods = response.body;
+              this.$http.get("/users/"+order.userId).then(response => {
+                order.user = response.body;
+                orderList.push(order);
               });
-            }
-          },
-          function(){
-            vux.alert.show({
-              title: '提示',
-              content: '获取订单失败！',
-              onHide () {
-                  window.history.back();
-              }
             });
-
-          }
-        )
-      },
-      methods:{
-        getButton(order){
-          var button = {
-            style: 'default',
-            text:"订单已完成",
-            onButtonClick: function() {
-            }
-          }
-
-          var httpClient = this.$http;
-          var vux = this.$vux;
-          if("WAIT_FOR_PAY" == order.status){
-            button.style = "primary";
-            button.text = "点击确认收到用户付款";
-
-            button.onButtonClick = function () {
-              httpClient.post('/orders/action/userPayed/orderId/'+order.id,{}).then(
-                function(response){
-                   if("0" == response.body.resultCode){
-                     vux.alert.show({
-                       title: '提示',
-                       content: '操作成功！',
-                       onHide () {
-                          window.location.reload();
-                       }
-                     });
-                   }else{
-                     vux.alert.show({
-                       title: '提示',
-                       content: '操作失败！'
-                     });
-                   }
-                },
-                function(){
-                   vux.alert.show({
-                     title: '提示',
-                     content: '操作失败！'
-                   });
-                }
-              )
-            }
-          }
-          if("PAY_SUCCESS" == order.status){
-            button.style = "primary";
-            button.text = "点击发货";
-            button.onButtonClick =function() {
-              button.onButtonClick = function () {
-                httpClient.post('/orders/action/deliverGoods/orderId/'+order.id,{}).then(
-                  function(response){
-                     if("0" == response.body.resultCode){
-                       vux.alert.show({
-                         title: '提示',
-                         content: '操作成功！',
-                         onHide () {
-                            window.location.reload();
-                         }
-                       });
-                     }else{
-                       vux.alert.show({
-                         title: '提示',
-                         content: '操作失败！'
-                       });
-                     }
-                  },
-                  function(){
-                     vux.alert.show({
-                       title: '提示',
-                       content: '操作失败！'
-                     });
-                  }
-                )
-              }
-            }
-          }
-          if("DILIVERED" == order.status){
-            button.style = "primary";
-            button.text = "点击确认用户收到商品";
-            button.onButtonClick =function() {
-              button.onButtonClick = function () {
-                httpClient.post('/orders/action/receiveGoods/orderId/'+order.id,{}).then(
-                  function(response){
-                     if("0" == response.body.resultCode){
-                       vux.alert.show({
-                         title: '提示',
-                         content: '操作成功！',
-                         onHide () {
-                            window.location.reload();
-                         }
-                       });
-                     }else{
-                       vux.alert.show({
-                         title: '提示',
-                         content: '操作失败！'
-                       });
-                     }
-                  },
-                  function(){
-                     vux.alert.show({
-                       title: '提示',
-                       content: '操作失败！'
-                     });
-                  }
-                )
-              }
-            }
-          }
-          return [button]
-        }
+          });
+        });
       }
     }
+  }
 </script>
 
 <style>
+  @import "../assets/css/goods_list.css";
+</style>
 
+<style>
+.vux-step-item-title {
+  font-size: .4rem !important;
+}
 </style>
